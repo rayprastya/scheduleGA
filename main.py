d4f1ba71a98b808c190f1fce0ee61daf6803478a
rayprastya <rayhan.prastya@gmail.com>
2023-05-16 11:40:18 +0700

    initialize GA

---
+++ main.py
@@ -0,0 +1,89 @@
+import prettytable 
+
+from schedule import Schedule
+from genetic import GeneticOptimize
+
+
+def vis(schedule):
+    """visualization Class Schedule.
+
+    Arguments:
+        schedule: List, Class Schedule
+    """
+    # 7 ke kanan
+    # 7 kebawah
+    col_labels = ['jam/slot', '7', '8', '9', '10', '11','12','13']
+    table_vals = [[i + 4, '', '', '', '', '','','',] for i in range(7)]
+
+    table = prettytable.PrettyTable(col_labels, hrules=prettytable.ALL)
+
+    for s in schedule:
+        weekDay = s.weekDay
+        slot = s.slot
+        text = 'id kelas: {} \n jenis kelas: {} \n room: {} \n matkul: {}'.format(s.classRoomId, s.categoryId, s.roomId, s.courseId)
+        table_vals[weekDay - 1][slot] = text
+
+    for row in table_vals:
+        table.add_row(row)
+
+    print(table)
+
+
+if __name__ == '__main__':
+    schedules = []
+
+    # add schedule
+    schedules.append(Schedule(201, "biasa", "METODOLOGI PENELITIAN"))
+    schedules.append(Schedule(201, "biasa", "SISTEM OPERASI"))
+    schedules.append(Schedule(202, "biasa", "PEMROGRAMAN VISUAL "))
+    schedules.append(Schedule(202, "biasa", "FINANCIAL TECHNOLOGY"))
+    schedules.append(Schedule(203, "biasa", "REKAYASA PERANGKAT LUNAK"))
+    schedules.append(Schedule(203, "biasa", "INTERAKSI MANUSIA"))
+    schedules.append(Schedule(206, "biasa", "E-BUSINESS"))
+    schedules.append(Schedule(206, "biasa", "PROYEK KONSULTANSI"))
+
+    schedules.append(Schedule(202, "khusus", "METODE NUMERIK"))
+    schedules.append(Schedule(202, "khusus", "SISTEM KONTROL"))
+    schedules.append(Schedule(204, "khusus", "TEKNIK SIMULASI "))
+    schedules.append(Schedule(204, "khusus", "TEKNIK RISET OPERASI"))
+    schedules.append(Schedule(206, "khusus", "SISTEM OPERASI"))
+    schedules.append(Schedule(206, "khusus", "SOSIAL DAN INOVASI MEDIA"))
+
+    schedules.append(Schedule(201, "khusus", "METODOLOGI PENELITIAN"))
+    schedules.append(Schedule(201, "khusus", "SISTEM OPERASI"))
+    schedules.append(Schedule(202, "khusus", "PEMROGRAMAN VISUAL"))
+    schedules.append(Schedule(202, "khusus", "FINANCIAL TECHNOLOGY"))
+    schedules.append(Schedule(203, "biasa", "REKAYASA PERANGKAT LUNAK"))
+    schedules.append(Schedule(203, "biasa", "INTERAKSI MANUSIA "))
+    schedules.append(Schedule(206, "biasa", "E-BUSINESS"))
+    schedules.append(Schedule(206, "biasa", "PROYEK KONSULTANSI"))
+
+    # schedules.append(Schedule(203, "biasa", ""))
+    # schedules.append(Schedule(203, "biasa", ""))
+    # schedules.append(Schedule(204, "biasa", ""))
+    # schedules.append(Schedule(204, "biasa", ""))
+    # schedules.append(Schedule(205, "biasa", ""))
+    # schedules.append(Schedule(205, "biasa", ""))
+    # schedules.append(Schedule(206, "biasa", ""))
+    # schedules.append(Schedule(206, "biasa", ""))
+
+    # optimization
+    ga = GeneticOptimize(popsize=50, elite=10, maxiter=100)
+    res = ga.evolution(schedules, 3)
+
+    # visualization
+    vis_res = []
+    for r in res:
+        print("id ruangan", r.categoryId)
+        print("class", r.classRoomId)
+        print("matkul", r.courseId)
+
+        print("room", r.roomId)
+        print("weekday", r.weekDay)
+        print("slot", r.slot)
+
+        print("===============\n")
+        # if r.classId == "eksekutif":
+        #     vis_res.append(r)
+        vis_res.append(r)
+    vis(vis_res)
---
+++ schedule.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+class Schedule:
+    """Class Schedule.
+    """
+    def __init__(self, classRoomId, categoryId, courseId):
+        """Init
+        Arguments:
+            classRoomId: int, unique flight id.
+            categoryId: int, unique class id.
+            courseId: int, unique destination id.
+        """
+        self.classRoomId = classRoomId
+        self.categoryId = categoryId
+        self.courseId = courseId
+
+        self.roomId = 0
+        self.weekDay = 0
+        self.slot = 0
+
+    def random_init(self, roomRange):
+        """random init.
+
+        Arguments:
+            roomSize: int, number of classrooms.
+        """
+        self.roomId = np.random.randint(1, roomRange + 1, 1)[0]
+        self.weekDay = np.random.randint(1, 8, 1)[0]
+        self.slot = np.random.randint(1, 8, 1)[0]
+
+
+def schedule_cost(population, elite):
+    """calculate conflict of class schedules.
+
+    Arguments:
+        population: List, population of class schedules.
+        elite: int, number of best result.
+
+    Returns:
+        index of best result.
+        best conflict score.
+    """
+    conflicts = []
+    n = len(population[0])
+
+    for p in population:
+        conflict = 0
+        for i in range(0, n - 1):
+            for j in range(i + 1, n):
+                # check flight in same time and same room 
+                if p[i].roomId == p[j].roomId and p[i].weekDay == p[j].weekDay and p[i].slot == p[j].slot:
+                    # print("ini konfliknya", p[i].roomId,  p[j].roomId)
+                    conflict += 1
+                # check flight for one class in same time
+                if p[i].categoryId == p[j].categoryId and p[i].weekDay == p[j].weekDay and p[i].slot == p[j].slot:
+                    conflict += 1
+                # check flight for one destination in same time
+                # if p[i].courseId == p[j].courseId and p[i].weekDay == p[j].weekDay and p[i].slot == p[j].slot:
+                #     conflict += 1
+                # check same flight for one class in same day
+                if p[i].categoryId == p[j].categoryId and p[i].classRoomId == p[j].classRoomId and p[i].weekDay == p[j].weekDay:
+                    conflict += 1
+
+        conflicts.append(conflict)
+
+    index = np.array(conflicts).argsort()
+
+    return index[: elite], conflicts[index[0]]
